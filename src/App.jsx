a7bb5ba5ba1f52386700696688339405c89be6ab
IgnacioMelgarejo <melgarejonacho@hotmail.com>
2023-06-06 10:31:25 -0300

    little change

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,7 @@ function App() {
 
   return (
     <main className='board'>
-      <h1>Ulises se la come doblada</h1>
+      <h1>Ta te ti</h1>
 
       <button onClick={resetGame}>Resetear juego</button>
 
